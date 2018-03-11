06bf3390faaa33c0e49640818dd41d2a65c79884
Matthew Houghton <matt@crandallce.com>
2018-03-11 16:35:05 -0600

    New databse models for time tracker

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class TimeEntry(models.Model):
+    date_and_time = models.DateTimeField()
+    project = models.ForeignKey(Project)
+
+class Project(models.Model):
+    company = models.CharField(max = 200)
+    
+
+

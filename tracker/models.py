42847f82d8a6a7ac88ee75f50d10e70ea7c12854
Matthew Houghton <matt@crandallce.com>
2018-03-26 22:39:25 -0600

    Migrations Made for time tracker info.

--- tracker/models.py
+++ tracker/models.py
@@ -1,20 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    projectnumber = models.CharField(max_length=200, primary_key=True)
+    company = models.CharField(max_length = 200)
+    projectyear = models.CharField(max_length = 10)
 class StartStopTimeEntry(models.Model):
     startTime = models.DateTimeField()
     endTime = models.DateTimeField()
-    project = models.ManytoManyField(Project)
+    project = models.ManyToManyField(Project)
 
 
 class DurationTimeEntry(models.Model):
     duration = models.DateTimeField()
     project = models.DateTimeField()
 
-class Project(models.Model):
-    projectnumber = models.CharField(max=200, primary_key=True)
-    company = models.CharField(max = 200)
-    projectyear = models.CharField(max = 10)
+
 
 
 

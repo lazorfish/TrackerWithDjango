de48a62790b77915e1712931ef1480de3e325a7c
Matthew Houghton <matt@crandallce.com>
2018-03-26 22:27:48 -0600

    Updates to database model

--- tracker/models.py
+++ tracker/models.py
@@ -1,12 +1,20 @@
 from django.db import models
 
 # Create your models here.
-class TimeEntry(models.Model):
-    date_and_time = models.DateTimeField()
-    project = models.ForeignKey(Project)
+class StartStopTimeEntry(models.Model):
+    startTime = models.DateTimeField()
+    endTime = models.DateTimeField()
+    project = models.ManytoManyField(Project)
+
+
+class DurationTimeEntry(models.Model):
+    duration = models.DateTimeField()
+    project = models.DateTimeField()
 
 class Project(models.Model):
+    projectnumber = models.CharField(max=200, primary_key=True)
     company = models.CharField(max = 200)
-    
+    projectyear = models.CharField(max = 10)
+
 
 
